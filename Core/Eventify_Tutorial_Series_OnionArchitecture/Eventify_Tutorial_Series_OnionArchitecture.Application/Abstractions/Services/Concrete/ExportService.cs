02d043a29bb4e9dee0802e821e97575735ae7931
Zeynep Kurı <113287020+ZeynepKURI@users.noreply.github.com>
2024-12-07 16:28:58 +0300

    güncel

--- Core/Eventify_Tutorial_Series_OnionArchitecture/Eventify_Tutorial_Series_OnionArchitecture.Application/Abstractions/Services/Concrete/ExportService.cs
+++ Core/Eventify_Tutorial_Series_OnionArchitecture/Eventify_Tutorial_Series_OnionArchitecture.Application/Abstractions/Services/Concrete/ExportService.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.Runtime.Serialization;
+using System.Security.Cryptography;
 using Eventify_Tutorial_Series_OnionArchitecture.Application.DTOs;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Eventify_Tutorial_Series_OnionArchitecture.Application.Abstractions.Services.Concrete
 {
 
-   //
-	public class ExportService
+    // Bu kodun amacı, bir dizi etkinlik (eventItems) bilgisini alıp, belirli bir formatta bir dosya olarak belirli bir konuma kaydetmektir. İşlevsel olarak, etkinlik verilerini bir metin formatında dışa aktarmayı sağlar.
+    // Kodun Amacı ve İşlevi Bağımlılıkları Yönetmek:Kodda Dependency Injection(DI) kullanılarak ITextService ve IFileService bağımlılıkları dışarıdan sağlanır.Bu, kodun esnekliğini artırır ve test edilebilirliği sağlar.
+//ITextService: Etkinlik verilerini(eventItems) belirli bir formatta metin haline getirir.
+//IFileService: Formatlanmış metni belirtilen bir dosya yoluna kaydeder.
+
+    public class ExportService
 	{
 		private readonly ITextService _textService;
 		private readonly IFileService _fileService;
