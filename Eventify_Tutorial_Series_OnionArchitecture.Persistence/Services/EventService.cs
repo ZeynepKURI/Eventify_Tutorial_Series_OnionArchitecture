f5adba4e5804ab2ead53ca0c1f3bd0a9e91e3977
Zeynep Kurı <113287020+ZeynepKURI@users.noreply.github.com>
2024-12-07 12:27:29 +0300

    07.12

--- Eventify_Tutorial_Series_OnionArchitecture.Persistence/Services/EventService.cs
+++ Eventify_Tutorial_Series_OnionArchitecture.Persistence/Services/EventService.cs
@@ -3,19 +3,47 @@ using Eventify_Tutorial_Series_OnionArchitecture.Application.Abstractions.Servic
 using Eventify_Tutorial_Series_OnionArchitecture.Application.DTO;
 using Eventify_Tutorial_Series_OnionArchitecture.Application.DTOs;
 using Eventify_Tutorial_Series_OnionArchitecture.Application.RequestParameters;
+using Eventify_Tutorial_Series_OnionArchitecture.Domain.Entities;
+using Eventify_Tutorial_Series_OnionArchitecture.Persistence.DbContexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace Eventify_Tutorial_Series_OnionArchitecture.Persistence.Services
 {
     public class EventService : IEventService
     {
-        public Task CreateEventAsync(CreateEventDTO createEventDTO)
+        private readonly AppDbContext _context; 
+
+        public EventService(AppDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public Task<IEnumerable<EventDTO>> GetAllEventsAsync(Pagination pagination)
+        public async Task CreateEventAsync(CreateEventDTO createEventDTO)
         {
-            throw new NotImplementedException();
+            var newEvent = new Event()
+            {
+                Title = createEventDTO.Title,
+                Date = createEventDTO.Date,
+                location= createEventDTO.location,
+            };
+            await _context.Events.AddAsync(newEvent);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<EventDTO>> GetAllEventsAsync(Pagination pagination)
+        {
+            return await _context.Events.Select(x=> new EventDTO()
+            {
+                Title = x.Title,
+                Date = x.Date,
+                location = x.location,
+
+             })
+                .Skip(pagination.PageCount=pagination.ItemCount)
+                .Take(pagination.ItemCount)
+                .ToListAsync();
+
         }
     }
 }
